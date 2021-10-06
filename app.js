93275a9107a420ff4414f0d5844dba615850954c
greentie12 <tdominguez@fordham.edu>
2021-10-06 16:32:38 -0400

    error handlers addition

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require("path");
 const app = express();
 const port = 4001;
 
+// require the projects and skills JSON data
 const { projects } = require("./data.json");
+const { skills } = require("./skills.json");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -22,15 +24,52 @@ app.get("/", (req, res, next) => {
 
 // GET about page
 app.get("/about", (req, res, next) => {
-  res.render("about");
+  res.render("about", { skills });
 });
 
 // GET project page
 app.get("/project/:id", (req, res, next) => {
   const projectId = req.params.id;
   const project = projects.find(({ id }) => id === +projectId);
+  // checks if the project exists in the projects object
+  if (project) {
+    res.render("project", { projects, id: projectId });
+  }
+  // error handler if the project id does not exists
+  else {
+    const err = new Error();
+    res.status(404);
+    err.message = "Looks like this page does not exist";
+    const errorMessage = err.message;
+    const code = res.statusCode;
+    res.render("page-not-found", { errorMessage, code });
+  }
+});
 
-  res.render("project", { project });
+// Error Handlers
+
+// 404 handler to catch a non-existent or undefined route
+app.use((req, res, next) => {
+  const err = new Error();
+  res.status(404);
+  err.message = "Not Found. Looks like this page does not exist";
+  const errorMessage = err.message;
+  const code = res.statusCode;
+  res.render("page-not-found", { errorMessage, code, err });
+});
+
+// Global handler to catch 404 or any other error
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).render("error");
+  } else {
+    res.status(500);
+    err.message =
+      "There is a problem with the resource you are looking for and can't currently be displayed.";
+    const errorMessage = err.message;
+    const code = res.statusCode;
+    res.render("error", { code, errorMessage });
+  }
 });
 
 // Port assigment
